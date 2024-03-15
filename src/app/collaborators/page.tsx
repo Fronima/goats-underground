60add99b4f4051f2be3f371787df9c2180f562f1
Frank Coleman <frankcoleman55@gmail.com>
2024-03-15 14:39:56 -0400

    fixed collaborator page

--- src/app/collaborators/page.tsx
+++ src/app/collaborators/page.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link"
 import { getCollaborators, Collaborator } from "../../../sanity/client"
-
-
-
 import React from "react"; // Import the React module
+import imageUrlBuilder  from "@sanity/image-url";
+import { client } from "../../../sanity/client";
+
+
+
+
 
 export default async function Collaborators(){
     const collaborators: Collaborator[] = await getCollaborators()
+    const builder = imageUrlBuilder(client)
 
     return (
         <div className="min-h-screen pt-20">
@@ -14,11 +18,12 @@ export default async function Collaborators(){
                 {
                     collaborators.map((collaborator) => (
                         <div key={collaborator.name} className="flex flex-col items-center justify-center p-4">
-                            <img src={collaborator.image} alt={collaborator.name} className="w-32 h-32 rounded-full"/>
-                            {collaborator.link &&
+                            <img src={builder.image(collaborator.image).url()} alt={collaborator.name} className="w-32 h-32"/>
+                            {collaborator.link ?
                             <Link href={collaborator.link}>
-                                <h1 className="text-center text-xl font-bold">{collaborator.name}</h1>
+                                <h1 className="text-center text-xl font-bold text-white">{collaborator.name}</h1>
                             </Link>
+                            : <h1 className="text-center text-xl font-bold text-white">{collaborator.name}</h1>
                             }
                         </div>
                     ))
